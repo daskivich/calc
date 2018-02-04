225e061892c1042c7552c6b197a203ac98888137
Dan Daskivich <daskivich.d@husky.neu.edu>
2018-02-04 18:28:02 -0500

    Completed calculator functionality and tests.

--- lib/calc.ex
+++ lib/calc.ex
@@ -1,6 +1,25 @@
 defmodule Calc do
   @moduledoc """
-  Documentation for Calc.
+  A module to emulate a calculator with four integer operations:
+  addition, subtraction, multiplication, (integer) division
+
+  intput types:
+    paren:
+      an input string that contains at least one set of parantheses
+    bin_op:
+      an input string that contains no parentheses and only one operation
+    mult_div:
+      an input string that contains no parentheses and more than one operation,
+      at least one of which is a multiplication or division
+    add_sub:
+      an input string that contains no parentheses and multiple addition and/or
+      subtraction operations, but no multiplications or divisions
+
+  input validity:
+    binary operators must have at least one space around them
+    the unary negative operator (e.g., -5) must have no spaces between
+    itself and its operand
+    multiple supurfluous parentheses must be avoided
   """
 
   @doc """
@@ -8,11 +27,21 @@ defmodule Calc do
 
   ## Examples
 
-  iex> Calc.hello
-  :world
+  iex> Calc.eval("2 * 4 + 5")
+  "13"
+
+  iex> Calc.eval("2 * (4 + 5)")
+  "18"
+
+  iex> Calc.eval("9 / 2")
+  "4"
+
+  iex> Calc.eval(" (  7 * ((3 +    2) - 8))")
+  "-21"
 
   """
 
+  # loop function to run the calculator
   def main(device \\ :stdio) do
     IO.gets(device, "> ")
     |> eval()
@@ -20,19 +49,79 @@ defmodule Calc do
     main()
   end
 
+  # evaluates a line of calculator input
   def eval(input) do
+    # format the input string
+    formatted = format(input)
+
+    # process input based on its type
+    case formatted |> type() do
+      :paren ->
+        formatted |> eliminateParen()
+      :bin_op ->
+        formatted |> binOp()
+      :mult_div ->
+        formatted |> eliminateMultDiv()
+      :add_sub ->
+        formatted |> eliminateAddSub()
+    end
+  end
+
+  # formats the given input string appropriately for evaluation
+  def format(input) do
+    # trim leading/trailing white space
     trimmed = input |> String.trim()
 
-    if trimmed |> String.contains?(")") do
-      [{firstClosedIndex, firstClosedLength} | restClosed] = Regex.run( ~r/\)/, trimmed, return: :index)
+    # change double spaces into single spaces
+    singleSpaced = singleSpace(trimmed)
 
-      open =
-        Regex.scan(~r/\(/, trimmed, return: :index)
-        |> Enum.map(fn ([first | rest]) -> first end)
+    # delete spaces after open parentheses
+    noSpaceOpenParen = Regex.replace(~r/\(\s/, singleSpaced, "(")
 
-      Enum.map(open, fn ({i, l}) -> IO.puts(i) end)
+    # delete spaces before closing parentheses
+    Regex.replace(~r/\s\)/, noSpaceOpenParen, ")")
+  end
 
-      matchingOpen = List.foldl(open, 0, fn ({i, l}, acc) ->
+  # recursive function to elimiate all multiple spaces in given input
+  def singleSpace(input) do
+    singleSpaced = Regex.replace(~r/\s\s/, input, " ")
+
+    if String.contains?(singleSpaced, "  ") do
+      singleSpace(singleSpaced)
+    else
+      singleSpaced
+    end
+  end
+
+  # determines the type of input given
+  def type(input) do
+    cond do
+      input |> String.contains?(")") ->
+        :paren
+      input |> String.split(" ") |> Kernel.length() <= 3 ->
+        :bin_op
+      input |> String.contains?("*") or input |> String.contains?("/") ->
+        :mult_div
+      true ->
+        :add_sub
+    end
+  end
+
+  # evaluates the first inner parenthetical operation
+  # and then recurses with the replaced value
+  def eliminateParen(input) do
+    # determine the index of the first closed parenthesis
+    [{firstClosedIndex, firstClosedLength} | restClosed] =
+      Regex.run( ~r/\)/, input, return: :index)
+
+    # determine the indexes of all opening parentheses
+    open =
+      Regex.scan(~r/\(/, input, return: :index)
+      |> Enum.map(fn ([first | rest]) -> first end)
+
+    # determine the index of the opening paran that matches the first closing
+    matchingOpen =
+      List.foldl(open, 0, fn ({i, l}, acc) ->
         if i > acc and i < firstClosedIndex do
           i
         else
@@ -40,121 +129,134 @@ defmodule Calc do
         end
       end)
 
-      IO.puts(trimmed)
-      IO.puts(Integer.to_string(matchingOpen))
-      IO.puts(Integer.to_string(firstClosedIndex))
-
-      eval(String.slice(trimmed, 0, matchingOpen) <>
-      eval(String.slice(trimmed, (matchingOpen + 1)..(firstClosedIndex - 1))) <>
-      String.slice(trimmed, (firstClosedIndex + 1)..(String.length(trimmed) - 1)))
+    # evaluate the expression inside the identified parentheses,
+    # insert this back into the input string, and recurse
+    if matchingOpen == 0 and firstClosedIndex == String.length(input) - 1 do
+      eval(String.slice(input, 1..(firstClosedIndex - 1)))
     else
-      parts = trimmed |> String.split(" ")
+      String.slice(input, 0, matchingOpen) <>
+      eval(String.slice(input, (matchingOpen + 1)..(firstClosedIndex - 1))) <>
+      String.slice(input, (firstClosedIndex + 1)..(String.length(input) - 1))
+      |> eval()
+    end
+  end
 
-      if Kernel.length(parts) <= 3 do
-        case parts do
-          [a, "+", b] ->
-            x = String.to_integer(a)
-            y = String.to_integer(b)
-            Integer.to_string(x + y)
-          [a, "-", b] ->
-            x = String.to_integer(a)
-            y = String.to_integer(b)
-            Integer.to_string(x - y)
-          [a, "*", b] ->
-            x = String.to_integer(a)
-            y = String.to_integer(b)
-            Integer.to_string(x * y)
-          [a, "/", b] ->
-            x = String.to_integer(a)
-            y = String.to_integer(b)
-            Integer.to_string(div(x, y))
-            x -> x
-        end
-      else
-        if String.contains?(trimmed, "*") or String.contains?(trimmed, "/") do
-          [{firstMultDivIndex, firstMultDivLength} | restClosed] = Regex.run(~r/(\*|\/)/, trimmed, return: :index)
+  # evaluates the given binary operation or unary expression
+  def binOp(input) do
+    case input |> String.split(" ") do
+      # addition
+      [a, "+", b] ->
+        x = String.to_integer(a)
+        y = String.to_integer(b)
+        Integer.to_string(x + y)
+      # subtraction
+      [a, "-", b] ->
+        x = String.to_integer(a)
+        y = String.to_integer(b)
+        Integer.to_string(x - y)
+      # multiplication
+      [a, "*", b] ->
+        x = String.to_integer(a)
+        y = String.to_integer(b)
+        Integer.to_string(x * y)
+      # division
+      [a, "/", b] ->
+        x = String.to_integer(a)
+        y = String.to_integer(b)
+        Integer.to_string(div(x, y))
+      # unary expression
+      x ->
+        x
+    end
+  end
 
-          leftSpace = firstMultDivIndex - 1
-          rightSpace = firstMultDivIndex + 1
+  # evaluates the first (from left to right) multiplication
+  # or division operation encountered
+  # and then recurses with the replaced value
+  def eliminateMultDiv(input) do
+    # determine the index of the first multiplication or division operator
+    [{firstMultDivIndex, firstMultDivLength} | restClosed] =
+      Regex.run(~r/(\*|\/)/, input, return: :index)
 
-          IO.puts("left space: " <> Integer.to_string(leftSpace))
-          IO.puts("right space: " <> Integer.to_string(rightSpace))
+    # set indexes for spaces surrounding this operator
+    leftSpace = firstMultDivIndex - 1
+    rightSpace = firstMultDivIndex + 1
 
-          spaces =
-            Regex.scan(~r/\s/, trimmed, return: :index)
-            |> Enum.map(fn ([{i, l} | rest]) -> i end)
+    # determine the indexes of all spaces in the input string
+    spaces =
+      Regex.scan(~r/\s/, input, return: :index)
+      |> Enum.map(fn ([{i, l} | rest]) -> i end)
 
-          Enum.map(spaces, fn (i) -> IO.puts(i) end)
-
-          {left, right} = List.foldl(spaces, {0, String.length(trimmed)}, fn (i, {l, r}) ->
-            if i > l and i < leftSpace do
-              {i, r}
-            else
-              if i < r and i > rightSpace do
-                {l, i}
-              else
-                {l, r}
-              end
-            end
-          end)
-
-          IO.puts(trimmed)
-          IO.puts(Integer.to_string(left))
-          IO.puts(Integer.to_string(right))
-
-          if left == 0 do
-            eval(String.slice(trimmed, left..(right - 1))) <>
-            String.slice(trimmed, right..(String.length(trimmed)))
-            |> eval()
-          else
-            String.slice(trimmed, 0..left) <>
-            eval(String.slice(trimmed, (left + 1)..(right - 1))) <>
-            String.slice(trimmed, right..(String.length(trimmed)))
-            |> eval()
-          end
+    # determine the indexes delimiting the operation identified
+    {left, right} =
+      List.foldl(spaces, {0, String.length(input)}, fn (i, {l, r}) ->
+        if i > l and i < leftSpace do
+          {i, r}
         else
-          [{firstAddSubIndex, firstAddSubLength} | restClosed] = Regex.run(~r/(\+|\-)/, trimmed, return: :index)
-
-          leftSpace = firstAddSubIndex - 1
-          rightSpace = firstAddSubIndex + 1
-
-          IO.puts("left space: " <> Integer.to_string(leftSpace))
-          IO.puts("right space: " <> Integer.to_string(rightSpace))
-
-          spaces =
-            Regex.scan(~r/\s/, trimmed, return: :index)
-            |> Enum.map(fn ([{i, l} | rest]) -> i end)
-
-          Enum.map(spaces, fn (i) -> IO.puts(i) end)
-
-          {left, right} = List.foldl(spaces, {0, String.length(trimmed)}, fn (i, {l, r}) ->
-            if i > l and i < leftSpace do
-              {i, r}
-            else
-              if i < r and i > rightSpace do
-                {l, i}
-              else
-                {l, r}
-              end
-            end
-          end)
-
-          IO.puts(trimmed)
-          IO.puts(Integer.to_string(left))
-          IO.puts(Integer.to_string(right))
-
-          if left == 0 do
-            eval(String.slice(trimmed, left..(right - 1))) <>
-            String.slice(trimmed, right..(String.length(trimmed)))
-            |> eval()
+          if i < r and i > rightSpace do
+            {l, i}
           else
-            String.slice(trimmed, 0..left) <>
-            eval(String.slice(trimmed, (left + 1)..(right - 1))) <>
-            String.slice(trimmed, right..(String.length(trimmed)))
-            |> eval()
+            {l, r}
           end
         end
-      end
+      end)
+
+    # evaluate the identified expression
+    # insert this back into the input string, and recurse
+    if left == 0 do
+      eval(String.slice(input, left..(right - 1))) <>
+      String.slice(input, right..(String.length(input)))
+      |> eval()
+    else
+      String.slice(input, 0..left) <>
+      eval(String.slice(input, (left + 1)..(right - 1))) <>
+      String.slice(input, right..(String.length(input)))
+      |> eval()
+    end
+  end
+
+  # evaluates the first (from left to right) addition
+  # or subtraction operation encountered
+  # and then recurses with the replaced value
+  def eliminateAddSub(input) do
+    # determine the index of the first addition or subtraction operator
+    [{firstAddSubIndex, firstAddSubLength} | restClosed] =
+      Regex.run(~r/(\+\s|\-\s)/, input, return: :index)
+
+    # set indexes for spaces surrounding this operator
+    leftSpace = firstAddSubIndex - 1
+    rightSpace = firstAddSubIndex + 1
+
+    # determine the indexes of all spaces in the input string
+    spaces =
+      Regex.scan(~r/\s/, input, return: :index)
+      |> Enum.map(fn ([{i, l} | rest]) -> i end)
+
+    # determine the indexes delimiting the operation identified
+    {left, right} =
+      List.foldl(spaces, {0, String.length(input)}, fn (i, {l, r}) ->
+        if i > l and i < leftSpace do
+          {i, r}
+        else
+          if i < r and i > rightSpace do
+            {l, i}
+          else
+            {l, r}
+          end
+        end
+      end)
+
+    # evaluate the identified expression
+    # insert this back into the input string, and recurse
+    if left == 0 do
+      eval(String.slice(input, left..(right - 1))) <>
+      String.slice(input, right..(String.length(input)))
+      |> eval()
+    else
+      String.slice(input, 0..left) <>
+      eval(String.slice(input, (left + 1)..(right - 1))) <>
+      String.slice(input, right..(String.length(input)))
+      |> eval()
     end
   end
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,59 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "2 + 3" do
+    assert Calc.eval("2 + 3") == "5"
+  end
+
+  test "5 * 1" do
+    assert Calc.eval("5 * 1") == "5"
+  end
+
+  test "20 / 4" do
+    assert Calc.eval("20 / 4") == "5"
+  end
+
+  test "24 / 6 + (5 - 4)" do
+    assert Calc.eval("24 / 6 + (5 - 4)") == "5"
+  end
+
+  test "1 + 3 * 3 + 1" do
+    assert Calc.eval("1 + 3 * 3 + 1") == "11"
+  end
+
+  test "1 - 4 - 5 - 6 - 7" do
+    assert Calc.eval("1 - 4 - 5 - 6 - 7") == "-21"
+  end
+
+  test "((24 / 6) + 3) * (5 - 4)" do
+    assert Calc.eval("((24 / 6) + 3) * (5 - 4)") == "7"
+  end
+
+  test "2  + 3" do
+    assert Calc.eval("2 + 3") == "5"
+  end
+
+  test "5 *  1" do
+    assert Calc.eval("5  * 1") == "5"
+  end
+
+  test " 5 * 1 " do
+    assert Calc.eval(" 5 * 1 ") == "5"
+  end
+
+  test "(5 * 2)" do
+    assert Calc.eval("(5 * 2)") == "10"
+  end
+
+  test "((5 * 2))" do
+    assert Calc.eval("(5 * 2)") == "10"
+  end
+
+  test "   (   5   *   2   )   " do
+    assert Calc.eval("   (   5   *   2   )   ") == "10"
+  end
+
+  test "24 / 6 + ( 5 - 4 )" do
+    assert Calc.eval("24 / 6 + ( 5 - 4 )") == "5"
   end
 end
